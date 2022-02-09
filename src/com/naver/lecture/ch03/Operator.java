464a6a1e78eb37004aa0dc404c4defb738e0b0f8
chakyungsuk <cha0054@naver.com>
2022-02-09 14:41:51 +0900

    ㅁㄴㅇㄹ

--- src/com/naver/lecture/ch03/Operator.java
+++ src/com/naver/lecture/ch03/Operator.java
@@ -3,8 +3,29 @@ package com.naver.lecture.ch03;
 public class Operator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// 연산의 방향
+		
+		
+		
+		// 연산의 우선순위
+			증감
+			산술
+			산술
+			쉬프트
+			비교
+			비교
+			논리
+			논리
+			논리
+			논리
+			논리
+			조건
+			대입
+		
+		// ***** ()로 적절히 우선 순위를 잡아서 사용하는 것이 중요 *****
+		
+		
 	}
 
 }
