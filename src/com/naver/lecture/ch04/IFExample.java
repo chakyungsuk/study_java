8a017e4b2ff5f45fe4ca0d62a85fb50ab40bc2c8
chakyungsuk <cha0054@naver.com>
2022-02-10 16:58:49 +0900

    ㅁㄴㅇㄹ

--- src/com/naver/lecture/ch04/IFExample.java
+++ src/com/naver/lecture/ch04/IFExample.java
@@ -10,7 +10,9 @@ public class IFExample {
 			System.out.println("점수가 90 보다 큽니다");
 			System.out.println("등급은 A 입니다.");
 		}
-
+		
+// 		블럭을 구성하지 않으면 바로 다음줄까지만 블럭내의 문장으로 이해하고 실행 시킨다.
+		
 		if(score < 90) 
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B 입니다.");
