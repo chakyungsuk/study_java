d95d8b9f46f6b89ea1c0a6c55ceb7fe3eb02a263
chakyungsuk <cha0054@naver.com>
2022-02-10 17:39:51 +0900

    .asd

--- src/com/naver/lecture/ch04/IfNestedExample.java
+++ src/com/naver/lecture/ch04/IfNestedExample.java
@@ -3,8 +3,19 @@ package com.naver.lecture.ch04;
 public class IfNestedExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int score = 95;
+		String grade = "";
+		
+		if(score >= 90) {
+			if(score >=95) {
+				grade = "A+";
+			} else {
+				grade = "A";
+			}
+		}
 
+		System.out.println("학점음 : " + grade);
 	}
 
 }
