52c8cd7ca9aec0adb515d00a0fc340ca262f87ef
chakyungsuk <cha0054@naver.com>
2022-02-14 18:06:09 +0900

    ㅁㄴ

---
+++ src/com/naver/lecture/ch04/ContinueExample.java
@@ -0,0 +1,15 @@
+package com.naver.lecture.ch04;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			if(i%2 !=0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
