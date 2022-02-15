8dd97abeed4912cd0ca7f80ca3ae05e0f7b34970
chakyungsuk <cha0054@naver.com>
2022-02-15 17:35:40 +0900

    asdf

--- src/com/naver/lecture/ch05/ArrayCopyExample.java
+++ src/com/naver/lecture/ch05/ArrayCopyExample.java
@@ -6,10 +6,19 @@ public class ArrayCopyExample {
 		int[] arr1 = {1,2,3};
 		int[] arr2 = new int[5];
 		
-		for(int i=0; i<arr1.length; i++) {
-			arr2[i] = arr1[i];
+		for(int i=0; i<arr2.length; i++) {
+			System.out.println(arr2[i]);
 		}
 
+		
+		System.out.println();
+		
+		System.arraycopy(arr1, 0, arr2, 2, arr1.length);
+		
+		for(int i=0; i<arr2.length; i++) {
+			System.out.println(arr2[i]);
+		}
+		
 	}
 
 }
