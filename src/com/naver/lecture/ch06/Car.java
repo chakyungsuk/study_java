bd4cdb3a81639b0df8a2500c18f6672a54f1626a
chakyungsuk <cha0054@naver.com>
2022-02-17 17:16:10 +0900

    asd

--- src/com/naver/lecture/ch06/Car.java
+++ src/com/naver/lecture/ch06/Car.java
@@ -2,4 +2,13 @@ package com.naver.lecture.ch06;
 
 public class Car {
 
+	// field
+		String company = "Volvo";
+		String model = "XC90";
+		String color = "darkblue";
+	
+		int maxSpeed = 240; 
+		int Speed = 0;
+	// method
+	
 }
---
+++ src/com/naver/lecture/ch06/Member.java
@@ -0,0 +1,17 @@
+package com.naver.lecture.ch06;
+
+public class Member {
+
+	String name = "고양이";
+	int age = 6;
+	String gender = "남자";
+	String birthday = "2000-01-01";
+	int mobile = 01000000000;
+	String ID = "cha0054";
+	String password = "asd1234";
+	String address = "서울특별시 강서구";
+	String email = "cha0054@naver.com";
+	
+	
+	
+}
