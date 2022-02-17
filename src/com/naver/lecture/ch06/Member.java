2994324c04029317957136da6ef98b60f70302f1
chakyungsuk <cha0054@naver.com>
2022-02-17 17:41:08 +0900

    asdf

--- src/com/naver/lecture/ch06/Member.java
+++ src/com/naver/lecture/ch06/Member.java
@@ -1,17 +1,19 @@
 package com.naver.lecture.ch06;
 
+import java.util.Date;
+
 public class Member {
 
 	String name = "고양이";
 	int age = 6;
 	String gender = "남자";
-	String birthday = "2000-01-01";
+	Date birthday;
 	int mobile = 01000000000;
 	String ID = "cha0054";
 	String password = "asd1234";
 	String address = "서울특별시 강서구";
 	String email = "cha0054@naver.com";
 	
-	
-	
+
+
 }
