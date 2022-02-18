2689da187bbb1f1abcf0b328010d3b8a7f5c6023
chakyungsuk <cha0054@naver.com>
2022-02-18 14:44:46 +0900

    sdf

--- src/com/naver/lecture/ch06/Car.java
+++ src/com/naver/lecture/ch06/Car.java
@@ -6,14 +6,19 @@ public class Car {
 		String company = "Volvo";
 		String model = "XC90";
 		String color = "DarkBlue";
-	
 		int maxSpeed = 240; 
 		int Speed = 0;
 		
 	
-	// method
+	// method : 함수 : function
 		
+	// 속도올리기
+		public int speedUp(int a) {
+			
+			int finalSpeed = 0;
+			return finalSpeed;
+		}
 		
-		
+	// 기아 변경
 		
 }
--- src/com/naver/lecture/ch06/Carprogram.java
+++ src/com/naver/lecture/ch06/Carprogram.java
@@ -3,7 +3,8 @@ package com.naver.lecture.ch06;
 public class Carprogram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
