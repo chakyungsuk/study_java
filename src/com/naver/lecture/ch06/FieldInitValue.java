ebfa3c4d08f83a694126d0a7ca045dbe8edf325e
chakyungsuk <cha0054@naver.com>
2022-02-21 15:51:57 +0900

    ㅁㄴㅇ

---
+++ src/com/naver/lecture/ch06/FieldInitValue.java
@@ -0,0 +1,22 @@
+package com.naver.lecture.ch06;
+
+public class FieldInitValue {
+
+	
+	
+// field
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+	
+}
