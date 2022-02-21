6908c0f022fd31fdf32fc05afc56e542a4f9b99b
chakyungsuk <cha0054@naver.com>
2022-02-21 15:52:08 +0900

    ㅁㄴㅇ

---
+++ src/com/naver/lecture/ch06/NewCarExample.java
@@ -0,0 +1,13 @@
+package com.naver.lecture.ch06;
+
+public class NewCarExample {
+
+	public static void main(String[] args) {
+		
+		NewCar newCar = new NewCar();
+		
+		NewCar newCar2 = new NewCar("white", 3000);
+
+	}
+
+}
