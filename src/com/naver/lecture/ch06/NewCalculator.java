dd332b2e57fa123db5b1afd974c8b8c75c2c4f4e
chakyungsuk <cha0054@naver.com>
2022-02-22 16:53:10 +0900

    asdf

---
+++ src/com/naver/lecture/ch06/NewCalculator.java
@@ -0,0 +1,29 @@
+package com.naver.lecture.ch06;
+
+public class NewCalculator {
+
+	int plus(int x, int y) {
+		int result = x = y;
+		return result;
+	}
+	
+	double avg(int x, int y) {
+		double sum = plus(x, y);
+		double result = sum / 2;
+		return result;
+	}
+	
+	void execute() {
+		double result = avg(7, 10);
+		println("실행결과 : " + result);
+	}
+	
+	void println(String message) {
+		System.out.println(message);
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/com/naver/lecture/ch06/NewCalculatorExample.java
@@ -0,0 +1,14 @@
+package com.naver.lecture.ch06;
+
+public class NewCalculatorExample {
+
+	public static void main(String[] args) {
+		
+		NewCalculator newCalculator = new NewCalculator();
+
+		newCalculator.execute();
+		
+		
+	}
+
+}
