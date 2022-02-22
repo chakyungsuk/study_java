38f35fe56e2cb54ce62fd46f7e0f379e6812d002
chakyungsuk <cha0054@naver.com>
2022-02-22 16:22:59 +0900

    asd

--- src/com/naver/lecture/ch06/FourthCar.java
+++ src/com/naver/lecture/ch06/FourthCar.java
@@ -1,5 +1,36 @@
 package com.naver.lecture.ch06;
 
 public class FourthCar {
-
+	
+	//필드
+	int gas;
+	
+	//생성자
+	
+	//메소드
+	void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	boolean isLeftGas() {
+		if(gas==0) {
+			System.out.println("gas가 없습니다.");
+			return false;
+		}
+			System.out.println("gas가 있습니다.");
+			return true;
+	}
+	
+	void run() {
+		while(true) {
+			if(gas > 0) {
+				System.out.println("달립니다.(gas잔량:" + gas + ")");
+				gas = gas -1;
+			} else {
+				System.out.println("멈춥니다.(gas잔량:" + gas + ")");
+				return;
+			} 
+			
+		}
+	}
 }
--- src/com/naver/lecture/ch06/FourthCarExample.java
+++ src/com/naver/lecture/ch06/FourthCarExample.java
@@ -1,10 +1,24 @@
-package com.naver.lecture.ch06.exc;
+package com.naver.lecture.ch06;
 
 public class FourthCarExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		FourthCar fourthcar = new FourthCar();
+		
+		fourthcar.setGas(5);
+		
+		boolean gasgas = fourthcar.isLeftGas();
+		if(gasgas) {
+			System.out.println("출발합니다");
+			fourthcar.run();
+		}
+		
+		if(fourthcar.isLeftGas()) {
+			System.out.println("gas를 주입할 필요가 없습니다.");
+		} else {
+			System.out.println("gas를 주입하세요.");
+		}
 	}
 
 }
