e9ed6515e70f21e58613863ec30cb2897100971a
chakyungsuk <cha0054@naver.com>
2022-02-23 14:49:25 +0900

    sdf

---
+++ src/com/naver/lecture/ch06/ForthCalculator.java
@@ -0,0 +1,14 @@
+package com.naver.lecture.ch06;
+
+public class ForthCalculator {
+
+	static double pi = 3.14159;
+	
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x = y;
+	}
+}
---
+++ src/com/naver/lecture/ch06/SixCarExample.java
@@ -0,0 +1,14 @@
+package com.naver.lecture.ch06;
+
+public class SixCarExample {
+
+	public static void main(String[] args) {
+
+		SixthCar mycar = new SixthCar()
+		SixthCar yourcar = new SixthCar()
+				
+		mycar.run();
+		yourcar.run();
+	}
+
+}
