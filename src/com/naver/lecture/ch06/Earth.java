0a482a87f9897fb29997ded1c3ef82d6b2b85929
chakyungsuk <cha0054@naver.com>
2022-02-23 16:44:49 +0900

    sdf

---
+++ src/com/naver/lecture/ch06/Earth.java
@@ -0,0 +1,10 @@
+package com.naver.lecture.ch06;
+
+public class Earth {
+
+	static final int EARTH_RADIUS = 6400;
+	static final double EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+//	static {
+//		EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+//	}
+}
---
+++ src/com/naver/lecture/ch06/EarthExample.java
@@ -0,0 +1,12 @@
+package com.naver.lecture.ch06;
+
+public class EarthExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + "km");
+		System.out.println("지구의 반지름 : " + Earth.EARTH_SURFACE_AREA + "km^2");
+		System.out.println(Earth.EARTH_SURFACE_AREA);
+	}
+		Earth earth = new Earth();
+}
