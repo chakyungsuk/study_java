0d66774696207267cfa51f7b926f997ccdd3cce3
chakyungsuk <cha0054@naver.com>
2022-03-07 16:23:35 +0900

    asd

--- src/com/naver/lecture/ch08/Audio.java
+++ src/com/naver/lecture/ch08/Audio.java
@@ -2,22 +2,29 @@ package com.naver.lecture.ch08;
 
 public class Audio implements RemoteControl {
 
+	//필드
+	public int volume;
+	
 	@Override
 	public void turnOn() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Audio를 켭니다.");
 	}
 
 	@Override
 	public void turnOff() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Audio를 끕니다");
 	}
 
 	@Override
 	public void setVolume(int volume) {
-		// TODO Auto-generated method stub
-
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 Audio 볼륨 : " + volume);
 	}
 
 }
