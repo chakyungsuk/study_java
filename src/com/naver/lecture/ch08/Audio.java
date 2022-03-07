a5e816de912beaac970564f8ee08ea387bc0e51d
chakyungsuk <cha0054@naver.com>
2022-03-07 16:18:34 +0900

    zxc

---
+++ src/com/naver/lecture/ch08/Audio.java
@@ -0,0 +1,23 @@
+package com.naver.lecture.ch08;
+
+public class Audio implements RemoteControl {
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/naver/lecture/ch08/RemoteControl.java
+++ src/com/naver/lecture/ch08/RemoteControl.java
@@ -5,7 +5,7 @@ public interface RemoteControl {
 	public String name = "Cha";
 	
 	//상수
-	int MAX_VOLUME = 10;     // public static final 생략되어 있음
+	public static final int MAX_VOLUME = 10;     // public static final 생략되어 있음
 	int MIN_VOLUME = 0;
 
 	//추상 메소드	: 구현 클래스에서 무조건 오버라이드 해야 된다.
@@ -13,8 +13,8 @@ public interface RemoteControl {
 	void turnOff();
 	void setVolume(int volume);
 	
-	//디폴트 메소드
-	default void setMute(boolean mute) {
+	//디폴트 메소드 : public 생략가능
+	public default void setMute(boolean mute) {
 		if(mute) {
 			System.out.println("무음 처리합니다");
 		} else {
@@ -23,7 +23,7 @@ public interface RemoteControl {
 	}
 	
 	//정적 메소드 : public 생략가능
-	static void changeBattery() {
+	public static void changeBattery() {
 		System.out.println("건전지를 교환합니다.");
 	}
 
