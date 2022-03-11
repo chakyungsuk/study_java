a071a460bf59030ec6142222f64a68e3ec717d06
chakyungsuk <cha0054@naver.com>
2022-03-11 15:59:35 +0900

    ㅁㄴㅇㄹ

--- src/com/naver/lecture/JavaTest/Computer.java
+++ src/com/naver/lecture/JavaTest/Computer.java
@@ -2,12 +2,10 @@ package com.naver.lecture.JavaTest;
 
 public class Computer extends Calculator {
 
-	public double param2 = Constants.PAI_2;
-	
 	@Override
-	public double param1() {
-		System.out.println("");
-		return super.param1();
+	public double areaCircle(double r) {
+		System.out.println("Computer 객체의 원면적 구하는 프로그램 실행");
+		return Constants.PAI_2 * r * r;
 	}
-	
+
 }
