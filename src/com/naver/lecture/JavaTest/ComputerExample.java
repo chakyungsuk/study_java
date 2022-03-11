823ba7a68f99c714c5ac23e31bee9782b3a14eed
chakyungsuk <cha0054@naver.com>
2022-03-11 15:59:46 +0900

    ㅁㄴㅇㄹ

--- src/com/naver/lecture/JavaTest/ComputerExample.java
+++ src/com/naver/lecture/JavaTest/ComputerExample.java
@@ -3,9 +3,18 @@ package com.naver.lecture.JavaTest;
 public class ComputerExample {
 
 	public static void main(String[] args) {
-
+       
+		Calculator calculator = new Calculator();
 		Computer computer = new Computer();
-		
-		computer.param1();
-	}
+        int r = 7;
+        
+        System.out.println("원의 반지름: " + r);
+        System.out.println();
+        
+        System.out.println("원면적: " + calculator.areaCircle(r));
+        System.out.println();
+        
+        System.out.println("원면적: " + computer.areaCircle(r));
+        System.out.println();
+    }
 }
