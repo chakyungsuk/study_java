0ed051a3be1871a259ea7519cf8502540fa537f4
chakyungsuk <cha0054@naver.com>
2022-03-11 15:59:40 +0900

    ㅁㄴㅇㄹ

--- src/com/naver/lecture/JavaTest/Calculator.java
+++ src/com/naver/lecture/JavaTest/Calculator.java
@@ -2,10 +2,9 @@ package com.naver.lecture.JavaTest;
 
 public class Calculator {
 
-	Constants constants = new Constants();
-	double param = constants.PAI_1;
-	
-	public double param1 () {
-		return 3.14 * param * param;
+	public double areaCircle(double r) {
+		System.out.println("Calculator 객체의 원면적 구하는 프로그램 실행");
+		return Constants.PAI_1 * r * r;	
 	}
 }
+
