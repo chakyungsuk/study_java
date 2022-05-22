c4b232982ba1b19d4392a097098f7ec9e5627068
chakyungsuk <cha0054@naver.com>
2022-05-22 18:05:42 +0900

    qwe

--- src/com/naver/lecture/ch02/Hello.java
+++ src/com/naver/lecture/ch02/Hello.java
@@ -1,20 +1,12 @@
 package com.naver.lecture.ch02;
 
+
 public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello!!!!! WellCome to Hell!!!!!");
+	
 		
-		int a = 1; // 선언 및 대입
-		
-		int b = 2;
-		
-		int c = 0;
-		
-		c = a + b;
-		
-		System.out.println(c);
 		
 	}
 
